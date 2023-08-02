fa29ca47d18530c0e40bb1466b8d809ffe7572fb
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-08-02 15:30:10 +0100

    Query Parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from blog_type import BlogType
+from typing import Optional
 
 app = FastAPI()
 
@@ -8,8 +9,12 @@ def index():
     return {'message': 'Hello world!'}
 
 @app.get('/blog/all')
-def get_all_blogs():
-    return  { 'message': 'All blogs provided'}
+def get_all_blogs(page = 1, page_size: Optional[int] = None):
+    return  { 'message': f'All {page_size} blogs on page {page}'}
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    return { 'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}' }
 
 @app.get('/blog/type/{type}')
 def get_blog_type(type: BlogType):
