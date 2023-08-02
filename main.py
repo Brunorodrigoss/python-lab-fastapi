b4c71636398d4f8ced1bb6ad58e594ba7dba0b17
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-08-02 14:17:04 +0100

    Pre defined values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from blog_type import BlogType
 
 app = FastAPI()
 
@@ -9,6 +10,10 @@ def index():
 @app.get('/blog/all')
 def get_all_blogs():
     return  { 'message': 'All blogs provided'}
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return { 'message': f'Blog type {type}'}
 
 @app.get('/blog/{id}')
 def get_blog(id: int):
