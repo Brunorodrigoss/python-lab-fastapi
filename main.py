b51d759658b7201724cff4f141390b88ed38e9bf
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-08-03 10:37:54 +0100

    Status code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from blog_type import BlogType
 from typing import Optional
 
@@ -20,7 +20,12 @@ def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional
 def get_blog_type(type: BlogType):
     return { 'message': f'Blog type {type}'}
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message': f'Blog with id {id}'}
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return  { 'error': f'Blog {id} not found'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': f'Blog with id {id}'}
 
